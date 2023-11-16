6e20dc7f4d94ba6e61a494fa0fd6fd24bdc208a4
kimbamtol0304 <kimbamtol0304@gmail.com>
2023-11-16 23:55:47 +0900

    Update Body.js

--- alarm/components/Body.js
+++ alarm/components/Body.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Modal, TextInput } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 class Body extends Component {
+  state = {
+    modalVisible: false,
+    newBssid: '', // 추가
+    currentTodoId: null, // 현재 선택된 할 일의 ID
+  };
+
+  setModalVisible = (visible, currentBssid = '', currentTodoId = null) => {
+    this.setState({ modalVisible: visible, newBssid: currentBssid, currentTodoId });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         {this.props.todos.map((data) => (
           <View style={styles.todo} key={data.id}>
             <View style={styles.todoText}>
-              <TouchableOpacity style={styles.todoCheckbox} onPressOut={() => this.props.checkTodo(data.id)}>
+              <TouchableOpacity
+                style={styles.todoCheckbox}
+                onPressOut={() => this.props.checkTodo(data.id)}
+              >
                 {data.completed ? (
                   <MaterialCommunityIcons size={20} name="checkbox-marked-circle-outline" />
                 ) : (
@@ -18,11 +32,45 @@ class Body extends Component {
               </TouchableOpacity>
               <Text style={data.completed ? styles.todoCompleted : null}>{data.text}</Text>
             </View>
-            <TouchableOpacity onPressOut={() => this.props.deleteTodo(data.id)}>
-              <MaterialCommunityIcons style={styles.todoDelBtn} size={30} name="delete-outline" />
-            </TouchableOpacity>
+            <View style={styles.todoButtons}>
+              <TouchableOpacity onPressOut={() => this.props.deleteTodo(data.id)}>
+                <MaterialCommunityIcons style={styles.todoDelBtn} size={30} name="delete-outline" />
+              </TouchableOpacity>
+              <TouchableOpacity onPressOut={() => this.setModalVisible(true, data.bssid, data.id)}>
+                <Ionicons style={styles.todoAddBtn} size={30} name="md-wifi-outline" />
+              </TouchableOpacity>
+            </View>
           </View>
         ))}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            this.setModalVisible(!this.state.modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Enter BSSID:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="BSSID"
+                onChangeText={(newBssid) => this.setState({ newBssid })}
+                value={this.state.newBssid}
+              />
+              <TouchableOpacity
+                style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                  this.props.saveBssid(this.state.currentTodoId, this.state.newBssid); // 현재 선택된 할 일 항목의 BSSID 업데이트
+                }}
+              >
+                <Text style={styles.textStyle}>Save BSSID</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -53,10 +101,58 @@ const styles = StyleSheet.create({
   },
   todoDelBtn: {
     color: '#777',
+    marginRight: 10,
+  },
+  todoAddBtn: {
+    color: '#4169E1',
+  },
+  todoButtons: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   todoCompleted: {
     color: '#bbb',
     textDecorationLine: 'line-through',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginTop: 10,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
