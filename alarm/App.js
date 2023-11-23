fdb7e346b9cc2b48e58c2a641ebe64c8a47ac69d
kimbamtol0304 <kimbamtol0304@gmail.com>
2023-11-23 15:03:24 +0900

    1123_15:03

--- alarm/App.js
+++ alarm/App.js
@@ -1,36 +1,134 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import Task from './components/Task';
+import SignScreen from './components/Sign';
+import LogScreen from './components/Log';
+import TestScreen from './crawls/Test';
+import BackgroundAnimation from './anime/backgroundanime';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import messaging from '@react-native-firebase/messaging';
 
-function HomeScreen({navigation}) {
+const __USER = '__login__';
+
+async function logout(navigation) {
+  await AsyncStorage.removeItem(__USER);
+  navigation.push('Home');
+
+}
+
+async function loginCheck(navigation) {
+  // 비동기 작업 수행
+  try {
+    const value = await AsyncStorage.getItem(__USER);
+    if (value != null) {
+      setTimeout(() => {
+        navigation.replace('Menu', { currentUser: value });
+      }, 4000);
+    }
+    else {
+      setTimeout(() => {
+        navigation.replace('Log');
+      }, 4000);
+
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+function HomeScreen({ navigation }) {
+  useEffect(
+    () => {
+      loginCheck(navigation);
+    },
+    []);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="LogIn!" onPress={() => navigation.navigate('Main')}/>
+    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'black' }}>
+      <Text style={{ textAlign: 'center', fontSize: 60, color: '#fff' }}>Loading...</Text>
+      <Text style={{ textAlign: 'center', fontSize: 15, color: '#fff' }}>로딩이 너무 길어지면 앱을 재시작해주세요.</Text>
+      <BackgroundAnimation />
     </View>
+  );
+
+}
+function MenuScreen({ route, navigation }) {
+  const { currentUser } = route.params
+
+  return (
+    <ImageBackground source={require('./assets/images/background1.jpg')} style={{ flex: 1, width: '100%', height: '100%' }}>
+      <View style={{ height: '100%', flexDirection: 'column', justifyContent: "space-around", alignItems: 'center' }}>
+        <TouchableOpacity style={styles.link} onPress={() => navigation.navigate('Main', { currentUser: currentUser })}>
+          <Text style={styles.buttonText}>main</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.link} onPress={() => navigation.navigate('Test')}>
+          <Text style={styles.buttonText}>weather</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.link} onPress={() => logout(navigation)}>
+          <Text style={styles.buttonText}>logout</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
-function MainScreen() {
-return (
-    <Task/>
-)
+function MainScreen({ route, navigation }) {
+  const { currentUser } = route.params
+  return (
+    <Task name={currentUser} />
+  )
 }
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  // 앱 시작시 Firebase 및 FCM 초기화
+  useEffect(() => {
+    const initializeFirebase = async () => {
+      try {
+        await messaging().registerDeviceForRemoteMessages();
+        console.log('Firebase 성공');
+      } catch (error) {
+        console.error('Firebase 오류:', error);
+      }
+    };
+
+    initializeFirebase();
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen options={{ headerShown: true }} name="Test" component={TestScreen} />
+        <Stack.Screen name="Log" component={LogScreen} />
+        <Stack.Screen options={{ headerShown: true }} name="Main" component={MainScreen} />
+        <Stack.Screen options={{ headerShown: true }} name="Sign" component={SignScreen} />
+        <Stack.Screen name="Menu" component={MenuScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  link: {
+    borderWidth: 3,
+    borderColor: '#fff',
+    borderRadius: 10,
+    width: '80%',
+    height: '10%',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 50,
+  },
+});
 
 export default App;
--- alarm/components/Task.js
+++ alarm/components/Task.js
@@ -1,60 +1,168 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';  // Added Alert
 import Header from './Header';
 import Body from './Body';
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from '@react-native-firebase/firestore';
+import NetInfo from '@react-native-community/netinfo';
+import Clipboard from '@react-native-clipboard/clipboard';
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
 
+const Task = () => {
+  const [todos, setTodos] = useState([]);
+  const [currentip, setCurrentip] = useState('');
+  const [wifiIp, setWifiIp] = useState('');
 
+  const firestore = getFirestore();
+  const todosCollection = collection(firestore, 'todos');
 
-export default class Task extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-    };
-  }
-
-  addTodo = (todoText) => {
-    const newTodo = {
-      id: Date.now(),
-      text: todoText,
-      completed: false,
-    };
-
-    this.setState((prevState) => ({
-      todos: [newTodo, ...prevState.todos],
-    }));
+  const getTodos = async () => {
+    try {
+      const querySnapshot = await getDocs(todosCollection);
+      const todosData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setTodos(todosData);
+    } catch (e) {
+      console.error('할 일을 가져오는 중 오류 발생:', e);
+    }
   };
 
-  checkTodo = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, completed: !todo.completed };
+  useEffect(() => {
+    getTodos();
+
+    const handleFCMMessage = async (remoteMessage) => {
+      console.log('FCM Message:', remoteMessage);
+
+      const wifiIP = remoteMessage.data && remoteMessage.data.wifiIP;
+      const matchingTodos = todos.filter((todo) => todo.ip === wifiIP);
+
+      if (matchingTodos.length > 0) {
+        Alert.alert('Match', '할 일이 있지않나요?');
+      }
+    };
+
+    const unsubscribeOnMessage = messaging().onMessage(handleFCMMessage);
+
+    const unsubscribeNetInfo = NetInfo.addEventListener(async (state) => {
+      if (state.isConnected && state.isInternetReachable) {
+        console.log('Connected to Wi-Fi:', state.details);
+        const newWifiIP = state.details && state.details.ipAddress;
+
+
+
+        if (newWifiIP !== wifiIp) {
+          console.log('Wi-Fi IP 변경 :', newWifiIP);
+
+          try {
+
+            // 일치하는 할일 아이템 찾기
+            const matchingTodos = todos.filter((todo) => todo.ip === newWifiIP);
+
+            console.log('Matching Todos:', matchingTodos);
+
+            // 일치하는 할일이 있다면 알림 전송
+            if (matchingTodos.length > 0) {
+              const todoText = matchingTodos[0].text; // 여기에서는 첫 번째 일치하는 할일의 텍스트를 가져옴
+              // push notification
+              PushNotification.localNotification({
+                message: `할 일 알림 : ${todoText}`,
+              });
+            }
+          } catch (error) {
+            console.error('FCM 에러 :', error);
+          }
+          setWifiIp(newWifiIP);
         }
-        return todo;
-      }),
-    }));
+      }
+    });
+
+    return () => {
+      unsubscribeOnMessage();
+      unsubscribeNetInfo();
+    };
+  }, [todos, wifiIp]);
+
+  const handleCheckWifiIp = async () => {
+    try {
+      await Clipboard.setString(wifiIp);
+      console.log('Wi-Fi IP 복사 성공 :', wifiIp);
+    } catch (e) {
+      console.error('Wi-Fi IP 복사 중 오류 발생:', e);
+    }
   };
 
-  deleteTodo = (id) => {
-    this.setState((prevState) => ({
-      todos: prevState.todos.filter((todo) => todo.id !== id),
-    }));
+  const addTodo = async (todoText) => {
+    try {
+      const newTodo = {
+        text: todoText,
+        ip: currentip,
+        completed: false,
+      };
 
+      const docRef = await addDoc(todosCollection, newTodo);
+      setTodos((prevTodos) => [{ ...newTodo, id: docRef.id }, ...prevTodos]);
+    } catch (e) {
+      console.error('할 일 추가 중 오류 발생:', e);
+    }
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Todo App</Text>
-        <Header addTodo={this.addTodo} />
-        <Body todos={this.state.todos} checkTodo={this.checkTodo} deleteTodo={this.deleteTodo} />
-      </View>
-    );
-  }
-}
+  const checkTodo = async (id) => {
+    try {
+      const todoRef = doc(todosCollection, id);
+      await updateDoc(todoRef, { completed: true });
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? { ...todo, completed: true } : todo))
+      );
+    } catch (e) {
+      console.error('할 일 업데이트 중 오류 발생:', e);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      const todoRef = doc(todosCollection, id);
+      await deleteDoc(todoRef);
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    } catch (e) {
+      console.error('할 일 삭제 중 오류 발생:', e);
+    }
+  };
+
+  const saveip = async (todoId, newip) => {
+    try {
+      const todoRef = doc(todosCollection, todoId);
+      await updateDoc(todoRef, { ip: newip });
+      setCurrentip(newip);
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === todoId ? { ...todo, ip: newip } : todo))
+      );
+    } catch (e) {
+      console.error('IP 업데이트 중 오류 발생:', e);
+    }
+  };
+
+  const handlePrint = () => {
+    console.log(todos);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Todo App</Text>
+      <Header addTodo={addTodo} />
+      <Body
+        todos={todos}
+        checkTodo={checkTodo}
+        deleteTodo={deleteTodo}
+        saveip={saveip}
+        addTodo={addTodo}
+      />
+
+
+      <TouchableOpacity onPress={handleCheckWifiIp} style={styles.checkWifiIpButton}>
+        <Text style={styles.checkWifiIpButtonText}>Check WIFI-IP</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -69,6 +177,28 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginBottom: 20,
   },
+  printButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  printButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  checkWifiIpButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  checkWifiIpButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
 
-
+export default Task;
